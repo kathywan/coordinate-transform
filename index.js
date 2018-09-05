4035a10be173b103db6d5cd90d9cdeeb7547cafc
kathywan <kathywan@users.noreply.github.com>
2018-09-05 09:58:30 -0400

    test close

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const app = express()
 app.get('/proj4/:fromEpsg/:toEpsg/:x/:y/', proj4.transform)
 app.get('/postgis/:fromEpsg/:toEpsg/:x/:y/', postgis.transform)
 
-/*app.get('/close', (req, res) => {
+app.get('/close', (req, res) => {
     console.log('Closing the server...')
 
-server.close(() => {
-    console.log('--> Server call callback run !!')
+    server.close(() => {
+        console.log('--> Server call callback run !!')
 
-process.exit()
+    process.exit()
+    })
 })
-})*/
 
 const server = app.listen(process.env.PORT || 3000)
 server.postgis = postgis
