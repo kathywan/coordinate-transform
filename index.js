398b05f163c190bcfd6f63ed55e202d83d83a7f3
qwan <qwan@pivotal.io>
2018-09-04 22:33:44 -0400

    end point for terminate

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const proj4 = require('./src/proj4')
 const postgis = require('./src/postgis')
-const killproc = require('./src/killproc')
 
 const app = express()
 
 app.get('/proj4/:fromEpsg/:toEpsg/:x/:y/', proj4.transform)
 app.get('/postgis/:fromEpsg/:toEpsg/:x/:y/', postgis.transform)
-app.get('/killproc', killproc)
+
+/*app.get('/close', (req, res) => {
+    console.log('Closing the server...')
+
+server.close(() => {
+    console.log('--> Server call callback run !!')
+
+process.exit()
+})
+})*/
 
 const server = app.listen(process.env.PORT || 3000)
 server.postgis = postgis
