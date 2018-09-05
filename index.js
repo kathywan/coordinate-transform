4fb27212ff03c3e849c79e8b8e326bd23f06e3a0
kathywan <kathywan@users.noreply.github.com>
2018-09-04 21:20:49 -0400

    add killproc endpoint

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const proj4 = require('./src/proj4')
 const postgis = require('./src/postgis')
+const killproc = require('./src/killproc')
 
 const app = express()
 
 app.get('/proj4/:fromEpsg/:toEpsg/:x/:y/', proj4.transform)
 app.get('/postgis/:fromEpsg/:toEpsg/:x/:y/', postgis.transform)
+app.get('/killproc', killproc)
 
 const server = app.listen(process.env.PORT || 3000)
 server.postgis = postgis
