a366ec4e1a167a4273b91dd6182e68ee09f7c9d3
kathywan <kathywan@users.noreply.github.com>
2018-09-06 13:27:26 -0400

    demo test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/proj4/:fromEpsg/:toEpsg/:x/:y/', proj4.transform)
 app.get('/postgis/:fromEpsg/:toEpsg/:x/:y/', postgis.transform)
 
-app.get('/hello', (req, res) => res.send('2nd try, hope it works!'))
+app.get('/hello', (req, res) => res.send('great job everyone!'))
 
 app.get('/close', (req, res) => {
     console.log('Closing the server...')
